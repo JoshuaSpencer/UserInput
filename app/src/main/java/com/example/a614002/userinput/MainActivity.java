a64eb1232ca658e971b1a9b5c7c46772eae27bd5
614002 <joshua.spencer010203@gmail.com>
2018-04-30 10:23:44 -0500

    Untested Finished Code

--- app/src/main/java/com/example/a614002/userinput/MainActivity.java
+++ app/src/main/java/com/example/a614002/userinput/MainActivity.java
@@ -3,18 +3,20 @@ package com.example.a614002.userinput;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    String name, email;
-    int favoriteNumber;
+    String name, email, favoriteNumber;
 
     EditText nameInput;
     EditText emailInput;
-    EditText favoriteNumberInput;
+    Spinner favoriteNumberInput;
 
     Button submitButton;
 
@@ -25,7 +27,13 @@ public class MainActivity extends AppCompatActivity {
 
         nameInput = (EditText)findViewById(R.id.nameInput);
         emailInput = (EditText)findViewById(R.id.emailInput);
-        favoriteNumberInput = (EditText)findViewById(R.id.favoriteNumberInput);
+        favoriteNumberInput = (Spinner) findViewById(R.id.favoriteNumberInput);
+
+        ArrayAdapter<String> numberAdapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.numbers));
+        numberAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        favoriteNumberInput.setAdapter(numberAdapter);
+
 
         submitButton = (Button)findViewById(R.id.submitButton);
         submitButton.setOnClickListener(new View.OnClickListener() {
@@ -33,11 +41,11 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 name = nameInput.getText().toString();
                 email = emailInput.getText().toString();
-                favoriteNumber = Integer.valueOf(favoriteNumberInput.getText().toString());
+                favoriteNumber = favoriteNumberInput.getSelectedItem().toString();
 
                 showToast(name);
                 showToast(email);
-                showToast(String.valueOf(favoriteNumber));
+                showToast(favoriteNumber);
             }
         });
 
